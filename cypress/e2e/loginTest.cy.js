e10995e0207cb646241d780786bdf617f24ba4ab
Rafael Morais <rafaelm.morais@hotmail.com>
2024-10-29 13:17:30 -0300

    teste de login completo com os 4 testes funcionais

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -4,22 +4,36 @@ let dadosLogin
 context('Teste EBAC Shop -> Login', () => {
   beforeEach(() => {
     cy.visit('minha-conta')
-  });
-
-  before(() => {
     cy.fixture('fxtPerfil').then(perfil => {
       dadosLogin = perfil
     })
   });
 
-  it('Login -> Caminho Feliz', () => {
+  it('Login -> Deve fazer login com sucesso', () => {
     cy.fixture('fxtPerfil').then((dados) => {
       cy.login(dados.usuario, dados.senha)
     })
-    cy.get('.page-title').should('contain', 'Minha conta')
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, ')
   });
 
-  it('', () => {
+  it('Login -> Deve barrar o login com senha inválida', () => {
+    cy.fixture('fxtPerfil').then((dados) => {
+      cy.login(dados.usuario2, dados.senha2)
+    })
+    cy.get('.woocommerce-error').should('contain', 'Perdeu a senha?')
+  });
 
+  it('Login -> Deve barrar o login com usuário inexistente', () => {
+    cy.fixture('fxtPerfil').then((dados) => {
+      cy.login(dados.usuario3, dados.senha)
+    })
+    cy.get('.woocommerce-error').should('contain','não está registrado neste site.')
+  });
+
+  it('Login -> Deve validar que a senha é obrigatória para realizar o login', () => {
+    cy.fixture('fxtPerfil').then((dados) => {
+      cy.login(dados.usuario)
+    })
+    cy.get('.woocommerce-error').should('contain','O campo da senha está vazio.')
   });
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,7 @@
 Cypress.Commands.add('login', (usuario, senha) => {
     cy.get('#username').type(usuario)
-    cy.get('#password').type(senha, {log: false})
+    if (senha) { //Deixar a senha como campo opcional
+        cy.get('#password').type(senha, { log: false })
+    }
     cy.get('.woocommerce-form > .button').click()
-});
+})

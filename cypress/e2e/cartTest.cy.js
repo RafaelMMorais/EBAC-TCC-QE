8a81752a04594d2d8f2b379aa77a8fdf75735053
Rafael Morais <rafaelm.morais@hotmail.com>
2024-10-29 16:25:04 -0300

    testes de carrinho 2/4 concluidos

--- cypress/e2e/cartTest.cy.js
+++ cypress/e2e/cartTest.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import cartTest from "../support/page_objects/poProducts.page"
 
 /*
 Como cliente da EBAC-SHOP
@@ -14,7 +15,7 @@ Regras de negócio:
 
 let dadosLogin
 
-context('Teste EBAC Shop -> Login', () => {
+context('Teste EBAC Shop -> Carrinho de compras', () => {
   beforeEach(() => {
     cy.visit('minha-conta')
     cy.fixture('fxtPerfil').then(perfil => {
@@ -26,14 +27,37 @@ context('Teste EBAC Shop -> Login', () => {
   });
 
   it('Teste carrinho de compras -> Deve adicionar um produto ao carrinho com sucesso', () => {
+    cy.fixture('fxtProduct').then(dados => {
+      let prod = 1
+      cartTest.buscarProduto(dados[prod].nomeProduto)
+      cartTest.addProdutoCarrinho(
+        dados[prod].tamanho,
+        dados[prod].cor,
+        dados[prod].quantidade)
 
+      cy.get('.woocommerce-message').should('contain', dados[prod].nomeProduto)
+    });
   });
 
   it('Teste carrinho de compras -> Deve aumentar a quantidade de um produto dentro do carrinho', () => {
+    cy.fixture('fxtProduct').then(dados => {
+      let prod = 1
+      cartTest.buscarProduto(dados[prod].nomeProduto)
+      cartTest.addProdutoCarrinho(
+        dados[prod].tamanho,
+        dados[prod].cor,
+        dados[prod].quantidade)
 
+      cy.get('.woocommerce-message').should('contain', dados[prod].nomeProduto)
+      cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+      cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
+      cy.get('.quantity > .input-text').clear().type("3")
+      cy.get('.pull-right > .btn').click()
+      cy.get('.woocommerce-message').should('contain', 'Carrinho atualizado.')
+    });
   });
 
-  it('Teste carrinho de compras -> Deve apresentar erro ao tentar ultrapassar o valor de R$990', () => {
+  it.only('Teste carrinho de compras -> Deve apresentar erro ao tentar ultrapassar o valor de R$990', () => {
 
   });
 
--- cypress/support/page_objects/poProducts.page.js
+++ cypress/support/page_objects/poProducts.page.js
@@ -1,19 +1,19 @@
-class produtosPage{
-    visitarUrl(){
+class cartTest {
+    visitarUrl() {
         cy.visit('produtos')
     }
 
-    buscarProduto(nomeProduto){
+    buscarProduto(nomeProduto) {
         cy.get('[name="s"]').eq(1).type(nomeProduto)
         cy.get('.button-search').eq(1).click()
     }
 
-    addProdutoCarrinho(tamanho, cor, quantidade){
-        cy.get('.button-variable-item-' + tamanho).click()
-        cy.get(`.button-variable-item-${cor}`).click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
+    addProdutoCarrinho(tamanho, cor, quantidade) {
+        cy.get('.button-variable-item-' + tamanho).should('exist').click( {force: true} );
+        cy.get(`.button-variable-item-${cor}`).should('be.visible').click();
+        cy.get('.input-text').clear().type(quantidade);
+        cy.get('.single_add_to_cart_button').click();
     }
 }
 
-export default new produtosPage()
+export default new cartTest()
